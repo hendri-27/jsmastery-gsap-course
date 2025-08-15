8d9d3c411557389b0603af0a3af268fd5f6a0a38
hendri-27 <62458850+hendri-27@users.noreply.github.com>
2025-08-15 13:40:17 +0700

    8a expo easing

--- pages/08a_expo-easing/script.js
+++ pages/08a_expo-easing/script.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 // Select the main floating action button (FAB)
 const fab = document.querySelector(".fab");
 
@@ -21,6 +23,8 @@ fab.addEventListener("click", () => {
     const arcSpan = Math.PI / 1.5; // radians
     const startAngle = Math.PI / 2 + arcSpan / 2; // start at top-center
 
+    const coords = [];
+
     children.forEach((child, i) => {
       // Distribute each child evenly across the arc
       const angle = startAngle - i * (arcSpan / (children.length - 1));
@@ -30,10 +34,47 @@ fab.addEventListener("click", () => {
       const y = -Math.sin(angle) * radius;
 
       // 🔜 Animation will go here
+      // gsap.to(child, {
+      //   x,
+      //   y,
+      //   opacity: 1,
+      //   rotate: 360,
+      //   duration: 0.5,
+      //   ease: "expo.out",
+      //   stagger: 1,
+      // });
+      coords[i] = { x, y };
+    });
+
+    // i move the gsap to outside loop for implement stagger
+    gsap.to(children, {
+      x: (i) => coords[i].x,
+      y: (i) => coords[i].y,
+      opacity: 1,
+      scale: 1,
+      rotate: 360,
+      duration: 0.5,
+      ease: "expo.out",
+      stagger: {
+        each: 0.15,
+        from: "start",
+      },
     });
   } else {
     children.forEach((child) => {
       // 🔜 Collapse animation will go here
+      gsap.killTweensOf(child); // will kill all the delayed animation
+
+      gsap.to(child, {
+        x: 0,
+        y: 0,
+        opacity: 0,
+        scale: 0.2,
+        rotate: 0,
+        duration: 0.3,
+        ease: "expo.in",
+        pointerEvents: "none",
+      });
     });
   }
 });
