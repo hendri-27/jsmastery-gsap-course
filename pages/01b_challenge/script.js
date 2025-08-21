91076f89314c7643e3c2dcf43ee0b92f9082e200
hendri-27 <62458850+hendri-27@users.noreply.github.com>
2025-08-21 16:53:32 +0700

    1b challenge add original answer

--- pages/01b_challenge/script.js
+++ pages/01b_challenge/script.js
@@ -1,7 +1,30 @@
-gsap.to('.card', {
-  ease: "power1.inOut", // for animation smoothness
-  boxShadow: "0 0px 50px rgba(255, 255, 255, 1)", // change the boxshadow end state
-  repeat: -1, // make it loop infinite
-  yoyo: true, // make it back and forth between start state and end state
-  duration: 2, // make duration animation is 2s
-})
+gsap.to(".card", {
+  opacity: 1,
+  scale: 1,
+  duration: 5,
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -20,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    });
+
+    // pulsing glow animation
+    gsap.to(".card", {
+      boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+    });
+  },
+});
+
+// my answer
+// gsap.to(".card", {
+//   ease: "power1.inOut", // for animation smoothness
+//   boxShadow: "0 0px 50px rgba(255, 255, 255, 1)", // change the boxshadow end state
+//   repeat: -1, // make it loop infinite
+//   yoyo: true, // make it back and forth between start state and end state
+//   duration: 2, // make duration animation is 2s
+// });
