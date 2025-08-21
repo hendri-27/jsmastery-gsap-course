50fac851229b040a3ac0ab50a77410c4a3010444
hendri-27 <62458850+hendri-27@users.noreply.github.com>
2025-08-21 17:08:38 +0700

    2a understanding properties with original solution

--- pages/02a_understanding-properties/script.js
+++ pages/02a_understanding-properties/script.js
@@ -1,15 +1,31 @@
 import gsap from "gsap";
 
-gsap.to('.box', {
+const repeat = document.querySelector(".repeat");
+
+const animation = gsap.to(".box", {
   opacity: 1,
   rotation: 360,
-  background: "#ff6f61",
-  borderRadius: '50%',
+  backgroundColor: "#ff6f61",
+  borderRadius: "50%",
   scale: 1.25,
   duration: 2,
-  ease: "bounce",
-  repeat: -1,
-  yoyo: true,
-  repeatDelay: 2,
-  stagger: 1,
-})
+});
+
+repeat.addEventListener("click", () => {
+  animation.restart();
+});
+
+// My answer
+// gsap.to('.box', {
+//   opacity: 1,
+//   rotation: 360,
+//   background: "#ff6f61",
+//   borderRadius: '50%',
+//   scale: 1.25,
+//   duration: 2,
+//   ease: "bounce",
+//   repeat: -1,
+//   yoyo: true,
+//   repeatDelay: 2,
+//   stagger: 1,
+// })
